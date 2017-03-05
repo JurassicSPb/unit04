1293a7171a4743a266460e61306dad1e08c6f3e4
JurassicSPb[credential] <Jurassic5@yandex.ru>
2017-03-05 12:13:27 +0300

    task4 - test added

--- src/test/java/com/epam/java/se/task4/FilmCollectionTest.java
+++ src/test/java/com/epam/java/se/task4/FilmCollectionTest.java
@@ -2,14 +2,20 @@ package com.epam.java.se.task4;
 
 import org.junit.Test;
 
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by Мария on 05.03.2017.
  */
 public class FilmCollectionTest {
     private static final String PATH = "films_collection.ser";
+
     @Test
             (expected = NullPointerException.class)
-    public void nullPointerTest()throws Exception{
+    public void nullPointerTest() throws Exception {
         FilmCollection films = new FilmCollection();
         films.addFilm(null);
         films.removeFilmByName(null);
@@ -17,20 +23,30 @@ public class FilmCollectionTest {
         films.saveCollection(null, null);
         films.loadCollection(null);
     }
-        @Test
-    public void addFilmTest(){
 
- }
+    @Test
+    public void addFilmTest() {
+        FilmCollection films = new FilmCollection();
+
+        Film film = new Film("Titanic", "drama", 1998);
+        Actor actor = new Actor("Leo Dicaprio");
+        Actor actor2 = new Actor("Kate Winslet");
+
+        films.addFilm(film, actor, actor2);
+
+        films.printFilmCollection();
+
+        for (Map.Entry<Film, List<Actor>> entry : films.getFilmCollection().entrySet()) {
+            assertTrue(entry.getKey().equals(film));
+
+        }
+    }
 //    @Test
 //    public void addFilmTest(){
-//        FilmCollection films = new FilmCollection();
-//        films.addFilm(new Film("Titanic", "drama", 1998),
-//                new Actor("Leo Dicaprio"), new Actor("Kate Winslet"));
-//        films.addFilm(new Film("Lord of the rings", "fantasy", 2005),
-//                new Actor("aaa"), new Actor("bbb"), new Actor("ccc"));
+//    films.addFilm(new Film("Lord of the rings", "fantasy", 2005),
+//        new Actor("aaa"), new Actor("bbb"), new Actor("ccc"));
 //        films.addFilm(new Film("Dogma", "fantasy, drama", 1999),
 //                new Actor("Matt Damon"), new Actor("Ben Affleck"), new Actor("Salma Hayek"));
-//
 //        films.printFilmCollection();
 //
 //        films.removeFilmByName("titanic");
